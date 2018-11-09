73ff51c0dce0b24d53e80e89faae32f8a3109692
neha tiwari <nehatiwari25021992@gmail.com>
2018-11-09 19:29:21 +0530

    two way binding

--- src/flowers/list.js
+++ src/flowers/list.js
@@ -9,7 +9,8 @@ class List extends Component{
             { text : "Dafodil", price : 20 , quantity : 1},
             { text : "Daisy", price : 30, quantity: 1},
             { text : "Dianella", price : 40 , quantity : 1}
-        ]
+        ],
+        customQuantity : 1
     }
 
     increaseQuantity = () => {
@@ -21,10 +22,33 @@ class List extends Component{
         this.setState({newState})
     }
 
+    changeQuantitytoTwo = (q) => {
+        let newState = this.state.names.map((flower)=>{
+            flower.quantity = q;
+            return flower
+        })
+
+
+        this.setState({newState})
+    }
+
+    changeQuantity = (event) => {
+        this.state.customQuantity = event.target.value;
+        let newState = this.state.names.map((flower)=>{
+            flower.quantity = event.target.value;
+            return flower
+        })
+        this.setState({newState})
+
+    }
+
     render(){
             return (
                 <div className="listCss">
                 <button onClick={this.increaseQuantity}>Add More Flowers </button>
+                 ---- <button onClick={this.changeQuantitytoTwo.bind(this,2)} >Custom Quantity 2 </button>
+                 <br/>
+                 <label>Your Quatity : </label> <input type="number" min="1" value={this.state.customQuantity} onChange={this.changeQuantity} />
                     <h1>{this.props.season} Season Flower List</h1>
                     {
                             this.state.names.map((flower) => {
