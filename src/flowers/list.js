adf84f644f09e6112df419ceba79ddf8a7fbb476
neha tiwari <nehatiwari25021992@gmail.com>
2018-11-10 19:33:49 +0530

    update delete list elements using unique key

--- src/flowers/list.js
+++ src/flowers/list.js
@@ -1,19 +1,25 @@
 import React, { Component} from 'react';
 import Name from './name'
 import Cart from './cart'
+import UniqueId from 'react-html-id';
 
 class List extends Component{
 
-    state = {
-        names : [
-            { text : "Lily", price : 10, quantity : 1},
-            { text : "Dafodil", price : 20 , quantity : 1},
-            { text : "Daisy", price : 30, quantity: 1},
-            { text : "Dianella", price : 40 , quantity : 1}
-        ],
-        customQuantity : 1,
-        isAddToCart :  false,
-        totalCost : 0
+    constructor(){
+        super()
+        UniqueId.enableUniqueIds(this)
+
+        this.state = {
+            names : [
+                { id : this.nextUniqueId() , text : "Lily", price : 10, quantity : 1},
+                { id : this.nextUniqueId(), text : "Dafodil", price : 20 , quantity : 1},
+                { id : this.nextUniqueId() , text : "Daisy", price : 30, quantity: 1},
+                { id : this.nextUniqueId(), text : "Dianella", price : 40 , quantity : 1}
+            ],
+            customQuantity : 1,
+            isAddToCart :  false,
+            totalCost : 0
+        }
     }
 
     increaseQuantity = () => {
@@ -55,28 +61,32 @@ class List extends Component{
             isAddToCart : true
         })
         let total = 0
-        total = this.state.names.reduce((result,res)=>{
-            console.log(result)
-            console.log(res)
-            result.price+=res.price
-                return result
-        }).price;
-
+        const newFlowers = Object.assign([],this.state.names)
+        newFlowers.forEach((f)=>{
+                total+=f.price*f.quantity
+        })
         this.setState({totalCost : total})
-
-        console.log("********** ",total)
-
     }
 
-    changeSelectFlower = (dataFromChild) =>{
-        console.log(dataFromChild)
-        let newState = this.state.names.map((flower)=>{
-            if(flower.text === dataFromChild.text){
-                flower.quantity += 1;
-            }
-            return flower
+    changeSelectFlower = (id, e) =>{
+        console.log(id)
+        const index = this.state.names.findIndex((flower)=>{
+            return flower.id === id
         })
-        this.setState({newState})
+
+        let newFlower = Object.assign({}, this.state.names[index])
+        newFlower.quantity = e.target.value
+
+        const names = Object.assign([],this.state.names)
+        names[index] = newFlower
+
+        this.setState({names : names})
+    }
+
+    deleteSelectFlower = (index , e) => {
+        const newFlowers = Object.assign([],this.state.names)
+        newFlowers.splice(index,1)
+        this.setState({names:newFlowers})
     }
 
     render(){
@@ -87,11 +97,35 @@ class List extends Component{
                  <br/>
                  <label>Your Quatity : </label> <input type="number" min="1" value={this.state.customQuantity} onChange={this.changeQuantity} />
                     <h1>{this.props.season} Season Flower List</h1>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>
+                                    <h4>Name</h4>
+                                </th>
+                                <th>
+                                    <h4>Quantity</h4>
+                                </th>
+                                <th>
+                                    <h4>Price</h4>
+                                </th>
+                                <th>
+                                    <h4>Action</h4>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
                     {
-                            this.state.names.map((flower) => {
-                                 return <Name changeFlowerQuantity={this.changeSelectFlower} flowername={flower}></Name>
+                            this.state.names.map((flower,index) => {
+                                 return <Name 
+                                 key = {flower.id}
+                                 changeFlowerQuantity={this.changeSelectFlower.bind(this,flower.id)} 
+                                 deleteFlower = {this.deleteSelectFlower.bind(this,index)}
+                                 flowername={flower}></Name>
                             })
                     }
+                    </tbody>
+                    </table>
                     {
                         (()=>{
                             if(this.state.isAddToCart){
--- src/flowers/name.js
+++ src/flowers/name.js
@@ -2,11 +2,19 @@ import React from 'react';
 
 const Name = (props) => {
     if(props.flowername){
-        return ( <h3>{props.flowername.text} | 
-        <input type="number"  onChange={() => props.changeFlowerQuantity(props.flowername)} value={props.flowername.quantity} />  |
-                     {props.flowername.price * props.flowername.quantity}                   
-                    </h3>
-                    )
+        return ( <tr>
+            <td><h3>{props.flowername.text} </h3></td>
+            <td>
+                <input type="number"  onChange={props.changeFlowerQuantity} value={props.flowername.quantity} /> 
+             </td>
+             <td>   
+                    {props.flowername.price * props.flowername.quantity}                   
+             </td>
+             <td>
+                 <button onClick={props.deleteFlower}>Delete</button>
+             </td>
+        </tr>
+        )
     }else{
         return (<div> -- </div>)
     }
